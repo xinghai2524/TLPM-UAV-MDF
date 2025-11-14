9017f6712cd718c69667a478baa7c8eb3fdebb97
xinghai2524 <xinghai2524@outlook.com>
2025-11-14 16:56:47 +0800

    增加视频检测功能；修复已知问题

--- src/components/HomeView/RectDraw.vue
+++ src/components/HomeView/RectDraw.vue
@@ -1,6 +1,5 @@
 <script setup>
 const props = defineProps(['x1', 'y1', 'x2', 'y2', 'conf', 'cls', 'isShow'])
-const conf = props.conf.toString().slice(0, 4)
 </script>
 
 <template>
@@ -8,9 +7,9 @@ const conf = props.conf.toString().slice(0, 4)
     v-show="props.isShow"
     :style="{ top: `${props.y1 - 4.5}%`, left: `${props.x1}%` }"
     class="absolute bg-techGreen/90 text-black text-xs px-1 rounded font-thin"
-    :title="`${props.cls} (置信度: ${conf})`"
+    :title="`${props.cls} (置信度: ${props.conf.toFixed(2)})`"
   >
-    {{ props.cls }} {{ conf }}
+    {{ props.cls }} {{ props.conf.toFixed(2) }}
   </div>
   <div
     v-show="props.isShow"
@@ -21,6 +20,6 @@ const conf = props.conf.toString().slice(0, 4)
       height: `${(props.y2 - props.y1).toFixed()}%`,
     }"
     class="absolute border-2 border-techGreen border-glow bg-techGreen/10"
-    :title="`${props.cls} (置信度: ${conf})`"
+    :title="`${props.cls} (置信度: ${props.conf.toFixed(2)})`"
   ></div>
 </template>
